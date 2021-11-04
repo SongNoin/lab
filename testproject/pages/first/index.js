88f8a74c62aa890ae88b3451c054b7581bc455c0
SongNoin <as5427072@gmail.com>
2021-11-04 22:23:30 +0900

    모바일 폴더 생성

--- testproject/pages/first/index.js
+++ testproject/pages/first/index.js
@@ -1,3 +1,23 @@
+import { Modal } from "antd";
+
+import { useState } from "react";
 export default function FirstPage() {
-  return <></>;
+  function onTogleLogin() {
+    setIsLoginOpen((prev) => !prev);
+  }
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
+  return (
+    <>
+      <div onClick={onTogleLogin}>모달 열기</div>
+      {isLoginOpen && (
+        <Modal
+          visible={true}
+          cancelButtonProps={{ style: { display: "none" } }}
+          okButtonProps={{ style: { display: "none" } }}
+        >
+          <div>모달입니다</div>
+        </Modal>
+      )}
+    </>
+  );
 }
